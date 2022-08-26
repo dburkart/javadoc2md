61f77aa00471d76d1b6ca8409868c6ba1d3d70ba
Dana Burkart <dana.burkart@gmail.com>
2022-08-26 16:23:59 -0700

    Adds 3 more scanner unit tests.
     * TestScanJavadocStart (/**)
     * TestScanJavadocEnd (*/)
     * TestScanJavadocLine

--- scanner_test.go
+++ scanner_test.go
@@ -19,6 +19,51 @@ func SetupWithState(input string, state ScanFn) *Scanner {
 	return s
 }
 
+func TestScanJavadocStart(t *testing.T) {
+	input := `/**
+	* Test`
+	
+	s := SetupWithState(input, ScanJavadocStart)
+	
+	s.State(s)
+	token := <- s.Tokens
+	
+	if token.Type != TOK_JDOC_START {
+		t.Errorf("got %q, wanted %q", token.Type, TOK_JDOC_START)
+	}
+}
+
+func TestScanJavadocEnd(t *testing.T) {
+	input := "*/"
+	
+	s := SetupWithState(input, ScanJavadocEnd)
+	
+	s.State(s)
+	token := <- s.Tokens
+	
+	if token.Type != TOK_JDOC_END {
+		t.Errorf("got %q, wanted %q", token.Type, TOK_JDOC_END)
+	}
+}
+
+func TestScanJavadocLine(t *testing.T) {
+	input := `This is a line assumed to be in a Javadoc
+	* This is the next line`
+	
+	s := SetupWithState(input, ScanJavadocLine)
+	
+	s.State(s)
+	token := <- s.Tokens
+	
+	if token.Type != TOK_JDOC_LINE {
+		t.Errorf("got %q, wanted %q", token.Type, TOK_JDOC_LINE)
+	}
+	
+	if token.Lexeme != "This is a line assumed to be in a Javadoc" {
+		t.Errorf("got %q, wanted 'This is a line assumed to be in a Javadoc'", token.Lexeme)
+	}
+}
+
 func TestScanJavadocTag(t *testing.T) {
 	s := SetupWithState("@tag and other stuff", ScanJavadocTag)
 	
