1bf7c59c112209476125c2c2abab5094d921558c
Dana Burkart <dana.burkart@gmail.com>
2022-09-20 17:22:46 -0700

    Adds the beginnings of a Symbol struct.
    Work towards issue #17. This struct will track all of the fields
    relating to a particular symbol, including type, location, parent (if
    applicable), and more.

---
+++ internal/parser/symbol.go
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2022, Dana Burkart <dana.burkart@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+package parser
+
+type SymbolType int
+
+const (
+    SYM_TYPE_INVALID SymbolType = iota
+    SYM_TYPE_CLASS
+    SYM_TYPE_INTERFACE
+    SYM_TYPE_METHOD
+    SYM_TYPE_FIELD
+)
+
+type Symbol struct {
+    Type SymbolType
+    Name string                 // Short name
+    Package string
+    Parent string               // Fields, methods, inner classes
+    Location string
+}
