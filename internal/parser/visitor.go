09b73bd500be7020b3750b1ccab4f0fe7e4a71d7
Dana Burkart <dana.burkart@gmail.com>
2022-09-25 17:12:31 -0700

    Write out arguments in the order they are defined (in java).
    This fixes issue #18, however it introduces a regression where not every
    @param will be written out (e.g. params which don't map to arguments).

--- internal/parser/visitor.go
+++ internal/parser/visitor.go
@@ -7,188 +7,196 @@
 package parser
 
 import (
-    "os"
-    "path/filepath"
-    "strings"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type VisitorConfigOptions struct {
-    OutputDirectory string
-    SkipPrivateDefs bool
+	OutputDirectory string
+	SkipPrivateDefs bool
 }
 
 func VisitDocuments(options *VisitorConfigOptions, docs chan *Document) {
-    var documents []*Document
+	var documents []*Document
 
-    // The symbol visitor is special in that we want to visit _every_ document
-    // with this visitor before proceeding
-    symbolVisitor := SymbolVisitor{Symbols: make(map[string]Symbol)}
-    for {
-        doc, ok := <-docs
-        if !ok {
-            break
-        }
+	// The symbol visitor is special in that we want to visit _every_ document
+	// with this visitor before proceeding
+	symbolVisitor := SymbolVisitor{Symbols: make(map[string]Symbol)}
+	for {
+		doc, ok := <-docs
+		if !ok {
+			break
+		}
 
-        symbolVisitor.visit(doc)
-        documents = append(documents, doc)
-    }
+		symbolVisitor.visit(doc)
+		documents = append(documents, doc)
+	}
 
-    visitors := []Visitor{
-        &MarkdownVisitor{
-            OutputDirectory: options.OutputDirectory,
-            SkipPrivateDefs: options.SkipPrivateDefs,
-            Symbols:         symbolVisitor.Symbols,
-        },
-    }
+	visitors := []Visitor{
+		&MarkdownVisitor{
+			OutputDirectory: options.OutputDirectory,
+			SkipPrivateDefs: options.SkipPrivateDefs,
+			Symbols:         symbolVisitor.Symbols,
+		},
+	}
 
-    for _, v := range visitors {
-        for _, d := range documents {
-            if len(d.Blocks) == 0 {
-                continue
-            }
+	for _, v := range visitors {
+		for _, d := range documents {
+			if len(d.Blocks) == 0 {
+				continue
+			}
 
-            v.visit(d)
-        }
-    }
+			v.visit(d)
+		}
+	}
 }
 
 type Visitor interface {
-    visit(*Document) (bool, string)
+	visit(*Document) (bool, string)
 }
 
 type SymbolVisitor struct {
-    Symbols map[string]Symbol
+	Symbols map[string]Symbol
 }
 
 func (v *SymbolVisitor) visit(doc *Document) (err bool, description string) {
-    err = false
-    description = ""
+	err = false
+	description = ""
 
-    for i, block := range doc.Blocks {
-        symbol := Symbol{Type: block.Type, Package: doc.Package, Name: block.Name}
-        if i == 0 {
-            symbol.Location = block.Name
-            v.Symbols[block.Name] = symbol
-        } else {
-            symbolName := doc.Blocks[0].Name + "#" + block.Name
-            symbol.Location = symbolName
-            v.Symbols[symbolName] = symbol
-        }
-    }
+	for i, block := range doc.Blocks {
+		symbol := Symbol{Type: block.Type, Package: doc.Package, Name: block.Name}
+		if i == 0 {
+			symbol.Location = block.Name
+			v.Symbols[block.Name] = symbol
+		} else {
+			symbolName := doc.Blocks[0].Name + "#" + block.Name
+			symbol.Location = symbolName
+			v.Symbols[symbolName] = symbol
+		}
+	}
 
-    return
+	return
 }
 
 type MarkdownVisitor struct {
-    OutputDirectory string
-    SkipPrivateDefs bool
-    Symbols         map[string]Symbol
+	OutputDirectory string
+	SkipPrivateDefs bool
+	Symbols         map[string]Symbol
 }
 
 // Given a MixedText token list, return a string with all the parameters
 // evaluated.
 func (m *MarkdownVisitor) interpolateText(tokens MixedText) string {
-    interpolationArray := make([]string, len(tokens))
+	interpolationArray := make([]string, len(tokens))
 
-    for i := 0; i < len(tokens); i++ {
-        token := tokens[i]
+	for i := 0; i < len(tokens); i++ {
+		token := tokens[i]
 
-        switch token.Type {
-        case TOK_JDOC_NL:
-            interpolationArray[i] = "\n"
-        case TOK_JDOC_LINE:
-            interpolationArray[i] = strings.TrimSpace(token.Lexeme) + " "
-        case TOK_JDOC_PARAM:
-            str := ""
-            if token.Lexeme == "@code" {
-                str += "`"
-                str += strings.TrimSpace(tokens[i+1].Lexeme)
-                str += "`"
-                i++
-            }
+		switch token.Type {
+		case TOK_JDOC_NL:
+			interpolationArray[i] = "\n"
+		case TOK_JDOC_LINE:
+			interpolationArray[i] = strings.TrimSpace(token.Lexeme) + " "
+		case TOK_JDOC_PARAM:
+			str := ""
+			if token.Lexeme == "@code" {
+				str += "`"
+				str += strings.TrimSpace(tokens[i+1].Lexeme)
+				str += "`"
+				i++
+			}
 
-            if token.Lexeme == "@link" {
-                target := strings.TrimSpace(tokens[i+1].Lexeme)
-                symbol := m.Symbols[target]
-                // TODO: The name of the link should match a shortened symbol
-                //       name, not the target.
-                str += "[" + target + "](" + symbol.Location + ")"
-                i++
-            }
+			if token.Lexeme == "@link" {
+				target := strings.TrimSpace(tokens[i+1].Lexeme)
+				symbol := m.Symbols[target]
+				// TODO: The name of the link should match a shortened symbol
+				//       name, not the target.
+				str += "[" + target + "](" + symbol.Location + ")"
+				i++
+			}
 
-            interpolationArray[i] = str + " "
-        }
-    }
+			interpolationArray[i] = str + " "
+		}
+	}
 
-    return strings.TrimSpace(strings.Join(interpolationArray, ""))
+	return strings.TrimSpace(strings.Join(interpolationArray, ""))
 }
 
 func (m *MarkdownVisitor) visit(doc *Document) (err bool, description string) {
-    err = false
-    description = ""
-    needs_newline := false
+	err = false
+	description = ""
+	needs_newline := false
 
-    if m.SkipPrivateDefs && doc.Blocks[0].Attributes["visibility"] == "private" {
-        return
-    }
+	if m.SkipPrivateDefs && doc.Blocks[0].Attributes["visibility"] == "private" {
+		return
+	}
 
-    f, createErr := os.Create(filepath.Join(m.OutputDirectory, doc.Blocks[0].Name+".md"))
-    if createErr != nil {
-        err = true
-        description = createErr.Error()
-        return
-    }
-    defer f.Close()
+	f, createErr := os.Create(filepath.Join(m.OutputDirectory, doc.Blocks[0].Name+".md"))
+	if createErr != nil {
+		err = true
+		description = createErr.Error()
+		return
+	}
+	defer f.Close()
 
-    for i, v := range doc.Blocks {
-        heading := "## "
-        sectionName := "`" + v.Definition + "` {#" + v.Name + "}"
+	for i, v := range doc.Blocks {
+		heading := "## "
+		sectionName := "`" + v.Definition + "` {#" + v.Name + "}"
 
-        if i == 0 {
-            heading = "# "
-            sectionName = v.Name
-        }
+		if i == 0 {
+			heading = "# "
+			sectionName = v.Name
+		}
 
-        f.WriteString(heading + sectionName + "\n\n")
+		f.WriteString(heading + sectionName + "\n\n")
 
-        // Write out the definition separately if this is the first block
-        if i == 0 {
-            if doc.Package != "" {
-                f.WriteString("```java\n")
-                f.WriteString("import " + doc.Package + "." + v.Name + "\n```\n\n")
-            }
+		// Write out the definition separately if this is the first block
+		if i == 0 {
+			if doc.Package != "" {
+				f.WriteString("```java\n")
+				f.WriteString("import " + doc.Package + "." + v.Name + "\n```\n\n")
+			}
 
-            f.WriteString("```java\n" + v.Definition + "\n```\n\n")
-        }
+			f.WriteString("```java\n" + v.Definition + "\n```\n\n")
+		}
 
-        // Before writing out content, write out any deprecated admonitions
-        if ret, found := v.Tags["@deprecated"]; found {
-            f.WriteString(":::caution Deprecated\n\n")
-            f.WriteString(m.interpolateText(ret) + "\n\n")
-            f.WriteString(":::\n\n")
-        }
+		// Before writing out content, write out any deprecated admonitions
+		if ret, found := v.Tags["@deprecated"]; found {
+			f.WriteString(":::caution Deprecated\n\n")
+			f.WriteString(m.interpolateText(ret) + "\n\n")
+			f.WriteString(":::\n\n")
+		}
 
-        f.WriteString(m.interpolateText(v.Text))
-        f.WriteString("\n\n")
+		f.WriteString(m.interpolateText(v.Text))
+		f.WriteString("\n\n")
 
-        if len(v.Params) > 0 {
-            f.WriteString("* **Parameters:**" + "\n")
-            needs_newline = true
-        }
+		if len(v.Params) > 0 {
+			f.WriteString("* **Parameters:**" + "\n")
+			needs_newline = true
+		}
 
-        for key, value := range v.Params {
-            f.WriteString("\t* `" + key + "` - " + m.interpolateText(value) + "\n")
-        }
+		// TODO: I'm not sure what the best way to ensure we get everything
+		//       from Javadocs written out since it may contain more than we
+		//       captured.
+		for _, value := range v.Arguments {
+			if description, found := v.Params[value.Name]; found {
+				f.WriteString("\t* `" + value.Name + "` - " + m.interpolateText(description) + "\n")
+			} else {
+				f.WriteString("\t* `" + value.Name + "` - *Undocumented*\n")
+			}
 
-        if ret, found := v.Tags["@return"]; found {
-            f.WriteString("* **Returns:** " + m.interpolateText(ret) + "\n")
-            needs_newline = true
-        }
+		}
 
-        if needs_newline {
-            f.WriteString("\n")
-        }
-        needs_newline = false
-    }
-    return
+		if ret, found := v.Tags["@return"]; found {
+			f.WriteString("* **Returns:** " + m.interpolateText(ret) + "\n")
+			needs_newline = true
+		}
+
+		if needs_newline {
+			f.WriteString("\n")
+		}
+		needs_newline = false
+	}
+	return
 }
